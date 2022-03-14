464579d6548344ffd455531855fd0b062ba84655
Manuel Duran <durbonca@gmail.com>
2022-03-14 18:54:58 -0300

    first commit

---
+++ src/components/marvel/MarvelScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MarvelScreen = () => {
+  return (
+    <div>
+        <h1>
+            Marvel Screen
+        </h1>
+    </div>
+  )
+}
---
+++ src/components/search/SearchScreen.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SearchScreen = () => {
+  return (
+    <div>Search Screen</div>
+  )
+}
