23835fc65c85c64de59e161b77cfbf75e2aeabd0
Manuel Duran <durbonca@gmail.com>
2022-03-14 20:49:37 -0300

    get heroe by id

--- src/components/hero/HeroList.jsx
+++ src/components/hero/HeroList.jsx
@@ -1,14 +1,12 @@
 import { getHeroesByPublisher } from "../../selectors/getHeroByPublisher"
+import { HeroCard } from "./HeroCard"
 
 export const HeroList = ( { publisher } ) => {
 
     const heroes = getHeroesByPublisher(publisher)
     return (
-            <ul>
-                {heroes.map( hero => { return (
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>)})}
-            </ul>
+            <div className="row rows-cols-1 row-cols-md-3 g-3">
+                { heroes.map( hero => <HeroCard key={hero.id} {...hero} /> ) }
+            </div>
     )
 }
--- src/components/hero/HeroScreen.jsx
+++ src/components/hero/HeroScreen.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useParams, Navigate } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+  const { id : heroeId } = useParams();
+
+  const hero = getHeroById(heroeId);  
+  if(!hero) {
+    return <Navigate to='/' />
+  }
+
+  const { superhero, publisher, alter_ego, first_appearance, characters } = hero;
+
   return (
-    <h1>HeroScreen</h1>
+    <h1>{ superhero }</h1>
   )
 }
